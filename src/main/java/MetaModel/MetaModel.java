1074d7e8cab7a68d738f7767c0f4cde6da85f898
amichne <austinmichne@gmail.com>
2019-08-28 17:19:27 -0700

    Basic set of Model/MetaModel funcionality added, still much more to go

--- src/main/java/MetaModel/MetaModel.java
+++ src/main/java/MetaModel/MetaModel.java
@@ -1,4 +1,8 @@
 package MetaModel;
 
 public class MetaModel {
+    private final String topLevelDir = null;
+
+    public MetaModel(){};
+
 }
--- src/main/java/MetaModel/MetaModelUtils.java
+++ src/main/java/MetaModel/MetaModelUtils.java
@@ -1,4 +1,27 @@
 package MetaModel;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class MetaModelUtils {
+    private static List<String> getSubLevelDirs(String topLevelDir) throws IOException {
+        Stream<Path> walk = Files.walk(Paths.get(topLevelDir));
+        return walk.filter(Files::isDirectory).
+                map(Path::toString).collect(Collectors.toList());
+    }
+
+    private static List<String> getSubLevelDirs(String topLevelDir, String filter) throws IOException {
+        List<String> filtered = new ArrayList<>();
+        for (String dir : getSubLevelDirs(topLevelDir))
+            if (!dir.toLowerCase().contains(filter.toLowerCase()))
+                filtered.add(dir);
+        return filtered;
+    }
+
 }
